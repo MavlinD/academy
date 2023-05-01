67a4bcd6c9b4fd9c3a52e050010101b19dda7a16
Mavlin Dm <mavlind@list.ru>
2023-05-01 10:54:57 +0700

    add passw restore for admins

--- src/django_space/ads/admin.py
+++ src/django_space/ads/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 
 from src.django_space.ads.models import Ads, Image
 
-admin.site.register(Ads)
-admin.site.register(Image)
+# class AdminSite(AdminSite):
+#     site_header = 'Monty Python administration'
+
+
+@admin.register(Ads)
+class AdsAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "description")
+    list_filter = ("name",)
+    search_fields = ("name", "description")
+    ordering = ["name", "price"]
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ("ads", "path")
+    list_filter = ("ads",)
+    search_fields = ("ads", "path")
+    ordering = ["ads", "path"]
+    # raw_id_fields = ('parent',)
